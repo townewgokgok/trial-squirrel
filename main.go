1e65caa88b0e14c0fede7cd420828bb3631fef10
townewgokgok <townewgokgok@gmail.com>
2017-04-17 12:05:47 +0900

    yml設定ファイル、接続テスト

--- main.go
+++ main.go
@@ -2,18 +2,43 @@ package main
 
 import (
 	"database/sql"
+	"gopkg.in/yaml.v2"
 	_ "github.com/go-sql-driver/mysql"
 	//sq "github.com/Masterminds/squirrel"
+	"io/ioutil"
+	"github.com/pkg/errors"
+	"fmt"
 )
 
+type settings struct {
+	mysql struct {
+		host string
+		db string
+		user string
+		pass string
+	}
+}
+
+func mustLoadSettings(s *settings) {
+	data, err := ioutil.ReadFile("settings.yml")
+	if err != nil {
+		panic(errors.Wrap(err, "Failed to load settings"))
+	}
+	err = yaml.Unmarshal(data, s)
+	if err != nil {
+		panic(errors.Wrap(err, "Failed to unmarshall settings"))
+	}
+}
+
 func main() {
-	db, err := sql.Open("mysql", "user:pass@/dbname")
+	var s settings
+	mustLoadSettings(&s)
+
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@/%s", s.mysql.user, s.mysql.pass, s.mysql.host))
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
-
-	
 
 	//rows, err := db.Query("SELECT * FROM users")
 	//if err != nil {
